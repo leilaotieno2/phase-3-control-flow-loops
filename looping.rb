8a593101cfb8faecc2058fbe5c8b9b58b41c9b8a
leilaotieno2 <leila.otieno@student.moringaschool.com>
2023-07-05 10:34:29 +0300

    first commit

--- looping.rb
+++ looping.rb
@@ -1,6 +1,16 @@
 def happy_new_year
-  # your code here
+  countdown = 10
+
+  while countdown >= 1
+    puts countdown
+    countdown -= 1
+  end
+
+  puts "Happy New Year!"
 end
+
+happy_new_year
+
 
 # No need to modify this code! Use this to implement the fizzbuzz_printer method.
 def fizzbuzz(num)
@@ -15,10 +25,36 @@ def fizzbuzz(num)
   end
 end
 
+
 def fizzbuzz_printer
-  # your code here
+  (1..100).each do |num|
+    if num % 3 == 0 && num % 5 == 0
+      puts "FizzBuzz"
+    elsif num % 3 == 0
+      puts "Fizz"
+    elsif num % 5 == 0
+      puts "Buzz"
+    else
+      puts num
+    end
+  end
 end
 
+fizzbuzz_printer
+
+
+
 def reverse_string(str)
-  # your code here
+  reversed = ""
+
+  i = str.length - 1
+  while i >= 0
+    reversed += str[i]
+    i -= 1
+  end
+
+  return reversed
 end
+
+puts reverse_string("hello")
+
